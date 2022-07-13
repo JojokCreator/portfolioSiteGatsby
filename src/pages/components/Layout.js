7d3a440657b7cc857df8dddc8add05af8ec0d6bf
JojokCreator <joeyk2k@hotmail.com>
2022-07-13 13:42:26 +0100

    packagejson fix2

--- src/pages/components/Layout.js
+++ src/pages/components/Layout.js
@@ -2,16 +2,18 @@ import React from "react"
 import Navbar from "./Navbar"
 import * as styles from "../../styles/global.css"
 import { StaticImage } from "gatsby-plugin-image"
-import { ParallaxProvider } from "react-scroll-parallax"
+// import { ParallaxProvider } from "react-scroll-parallax"
 
 export default function Layout({ children }) {
   return (
-    <ParallaxProvider>
+    // <ParallaxProvider>
+    <>
       <Navbar />
       <div className={styles.content}>{children}</div>
       <footer>
         <p>Copyright J.Klakus 2022</p>
       </footer>
-    </ParallaxProvider>
+      </>
+    // </ParallaxProvider>
   )
 }
